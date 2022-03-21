20b04363de540c7b14578b2780e42d728bc87eb7
GLChamika <2019is025@stu.ucsc.lk>
2022-03-21 23:24:28 +0530

    Added the placekot.php

--- app/views/restaurants/placekot.php
+++ app/views/restaurants/placekot.php
@@ -10,10 +10,23 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/stylen.css">
+  <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/styled.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
   <script src="https://use.fontawesome.com/a6a11daad8.js"></script>
-  <title>Place KOT</title>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+function add_row()
+{
+ $rowno=$("#dynamic_table tr").length;
+ $rowno=$rowno+1;
+ $("#dynamic_table tr:last").after("<tr id='row"+$rowno+"'><td><select id='itemName' name='itemName[]'><?php foreach($data['fooditems'] as $fooditems): ?><option value='<?php echo $fooditems->itemName; ?>'><?php echo $fooditems->itemName; ?></option><?php endforeach; ?></select></td><td><select id='portion' name='portion[]'><option value='Small'>Small</option></select></td><td><input type='text' id='quantity' name='quantity[]'></td><td><input type='button' value='DELETE' onclick=delete_row('row"+$rowno+"')></td></tr>");
+}
+function delete_row(rowno)
+{
+ $('#'+rowno).remove();
+}
+</script>
+<title>Place KOT</title>
 </head>
 <body>
   <section class="system">
@@ -62,119 +75,57 @@
     </div>
 
     <div class="rest-dash-plus3">
-      <a href="<?php echo URLROOT; ?>/users/cashier">
-        <i class="fa fa-home fa-4x" aria-hidden="true"></i>
+      <a href="<?php echo URLROOT; ?>/restaurants/addfooditem">
+        <i class="fa fa-plus-square fa-4x" aria-hidden="true"></i>
       </a>
-      <p>Dashboard</p>
+      <p>Add Food Items</p>
     </div>
   </div>
-  
+
   <div class="sys-right-col">
-    <div class="KOT-right">
 
-    <!--Place KOT Heading -->
+  <div class="kot-div">
 
-    <div class="KOT-Heading">
-      <form action="sendkot.php">
+    <div class="dynamic_form">
+      <form method="post" action="<?php echo URLROOT; ?>/restaurants/placekot">
 
-        <div class="kot-form-head1">
-          <label for="resno">Reservation No:</label>
-          <input type="text" id="resno" name="resno">
-        </div>
+      <label for="tableno">Table No:</label>
+      <input type="text" id="tableno" name="tableno"><br><br>
 
-        <div class="kot-form-head2">
-          <label for="roomno">Room No:</label>
-          <input type="text" id="roomno" name="roomno">
-        </div><br>
+      <input type="button" onclick="add_row();" value="ADD ROW"><br><br>
 
-        <label for="kotno">KOT No:</label>
-        <input type="text" id="kotno" name="kotno"><br>
+        <table id="dynamic_table" align=center>
+        <tr id="row1">
+          <td>
+              <select id="itemName" name="itemName[]">
+              <?php foreach($data['fooditems'] as $fooditems): ?>
+              <option value="<?php echo $fooditems->itemName; ?>"><?php echo $fooditems->itemName; ?></option>
+              <?php endforeach; ?>
+              </select>
+          </td>
+          <td>
+              <select id="portion" name="portion[]">
+                <option value="Small">Small</option>
+                <option value="Regular">Regular</option>
+                <option value="Large">Large</option>
+              </select>
+          </td>
+          <td>
+              <input type="text" id="quantity" name="quantity[]" placeholder="Quantity">
+          </td>
+        </tr>
+        </table>
+              <select id="status" name="status" hidden>
+                <option value="pending" select=selected>pending</option>
+                <option value="prepared">prepared</option>
+                <option value="closed">closed</option>
+              </select>
+        <input type="submit" name="submit_row" value="SUBMIT">
       </form>
     </div>
-
-    <div class="kot-food-add-form">
-      
-      <form action="add.php">
-
-        <select id="name" name="name">
-          <option value="item">Select the Food Item</option>
-          <option value="item 1">item 1</option>
-          <option value="item 2">item 2</option>
-          <option value="item 3">item 3</option>
-        </select><br><br>
-
-        <input type="text" id="quantity" name="quantity" placeholder="Quantity"><br><br>
-
-        <select id="potion" name="portion">
-        <option value="Small">Small</option>
-          <option value="Normal">Normal</option>
-          <option value="large">large</option>
-        </select><br><br>
-      
-        <input type="submit" value="Add the Food Item">
-      
-      </form>
-    </div>
-
-    <!-- Room KOT Details -->
-    <div class="res-kot-detail-heading">       
-      <h2>KOT Details</h2>
-    </div>
-
-    <!-- KOT Item Details -->
-             
-            <div class="res-kot-details">
-            <table>
-              
-              <tr>
-              <th style="width:30%;">Food Item</th>
-              <th style="width:20%;">Quantity</th>
-              <th style="width:30%;">Portion</th>
-              <th style="width:10%;"></th>
-              <th style="width:10%;"></th>
-              </tr>
-              
-              <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td><a href="#"><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></a></td>
-              <td><a href="#"><i class="fa fa-trash-o fa-2x" aria-hidden="true"></i></a></td>
-              </tr>
-              <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td><a href="#"><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></a></td>
-              <td><a href="#"><i class="fa fa-trash-o fa-2x" aria-hidden="true"></i></a></td>
-              </tr>
-              <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td><a href="#"><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></a></td>
-              <td><a href="#"><i class="fa fa-trash-o fa-2x" aria-hidden="true"></i></a></td>
-              </tr>
-
-            </table><br>
-
-            <div class="recep-kot-form">
-      
-            <form action="sendkot.php">
-              <label for="snotes">Special Notes:</label><br>
-              <textarea name="" id="" cols="30" rows="8"></textarea><br><br>
-              
-              <div class="res-kot-details-place-button"> 
-                <a href="#roomplaceorder"><button type="submit">Send KOT</button></a>
-              </div><br><br>
-            </form>
-            
-            </div>
-                
-            </div>
-
 
   </div>
+
   </div>
 
 </body>
