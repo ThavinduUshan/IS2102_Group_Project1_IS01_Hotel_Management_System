8446b92bb83d3c8019c49d3ed03bcbea47b87427
Janani-Samarawickrama <2019is076@stu.ucsc.lk>
2022-03-22 22:53:18 +0530

    Added the queries of Update and delete of snack items

--- app/models/Kitchen.php
+++ app/models/Kitchen.php
@@ -35,13 +35,56 @@
       return $results;
     }
 
-    public function viewbarorderdetails(){
+     public function viewbarorderdetails(){
 
       $this->db->query('SELECT barorders.BarOrderNo, barorders.TableNo, barorders.Status FROM barorders INNER JOIN barordersnacks ON barorders.BarOrderNo = barordersnacks.BarOrderNo ORDER BY BarOrderNo ASC');
 
       $results = $this->db->resultSet();
 
+     return $results;
+    }
+    
+    public function viewrestaurantorderdetails($orderno){
+
+    // $this->db->query('SELECT fooditemid,PortionType,Quantity FROM restaurantorderitems ');
+    $this->db->query('SELECT restaurantorderitems.`PortionType`,restaurantorderitems.`Quantity`,fooditems.`itemName` FROM restaurantorderitems INNER JOIN fooditems ON fooditems.`fooditemId` = restaurantorderitems.`fooditemId` WHERE restaurantorderitems.`RestaurantOrderNo` = :orderno');
+    $this->db->bind(':orderno', $orderno);
+   
+
+     
+     $results = $this->db->resultSet();
       return $results;
     }
+    public function findItemById($fooditemId){
+      $this->db->query('SELECT * FROM fooditems WHERE fooditemId = :fooditemId');
+      $this->db->bind(':fooditemId', $fooditemId);
+      $row = $this->db->single();
+      return $row;
+     }
+     
+     public function updateAvailability($data){
+       $this->db->query('UPDATE fooditems SET status = :status WHERE fooditemId = :fooditemId');
+       
+       $this->db->bind(':fooditemId',$data['fooditemId']);
+       $this->db->bind(':status',$data['status']);
+  
+       if($this->db->execute()){
+        return true;
+      }else{
+        return false;
+      }
+     }
+     public function deleteItem($fooditemId){
+      $this->db->query('DELETE FROM fooditems  WHERE fooditemId = :fooditemId');
+  
+      $this->db->bind(':fooditemId',$fooditemId);
+      if($this->db->execute()){
+        return true;
+      }else{
+        return false;
+      }
+     }
+   
+    
 
   }
